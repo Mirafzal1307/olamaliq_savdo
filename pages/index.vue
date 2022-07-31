ca201aebccca7f551a4698af621cc98a4374cde9
eljahon <neljahon@gmail.com>
2022-07-31 22:06:19 +0500

    last commit

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,5 @@
 <template>
   <div>
-<<<<<<< HEAD
 <nuxt-child></nuxt-child>
-=======
-    <navbar />
-    <main>
-      <nuxt-child />
-    </main>
-    <footer />
->>>>>>> 63e37c4 (test case dashboard)
   </div>
 </template>
