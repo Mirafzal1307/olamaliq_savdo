c821e7b107fac1c33dab1fe8439b8abc7622b678
Asliddin2002 <asliddinwiutstudent@gmail.com>
2022-08-31 11:19:49 +0500

    registrartion

--- components/navbar/Navbar.vue
+++ components/navbar/Navbar.vue
@@ -1,81 +1,81 @@
 <template>
-<div  class="foo w-full top-0" >
-    <div class = "w-full shadow shadow-black-500/50 bg-white" >
-        <Container>
-            <div class="flex justify-between items-center py-4 gap-0.75 bg-white w-full">
-                <h1 class="font-semibold text-lg cursor-pointer "> <span class="border-b-2 border-green">Olmaliq</span>
-                    Savdo</h1>
-                <div
-                    class="katalog flex justify-center items-center py-1 px-5 cursor-pointer  gap-2 bg-green  text-white font-semibold rounded-md">
-                    <img src="../../assets/icons_svg/Menu.svg" alt="menu">
-                    <h4>{{$t('navbar.cat')}}</h4>
-                    <img src="../../assets/icons_svg/ArrowRight.svg" alt="arrow" />
-                </div>
-                <div class="search relative  w-7/12">
-                    <input type="text"
-                        class=" border-2 text-black-900 outline-none border-green pl-1 py-1 rounded-md w-full text-sm"
-                        placeholder="Mahsulot izlash...">
+    <div class="foo w-full top-0">
+        <div class="w-full shadow shadow-black-500/50 bg-white">
+            <Container>
+                <div class="flex justify-between items-center py-4 gap-0.75 bg-white w-full">
+                    <h1 class="font-semibold text-lg cursor-pointer "> <span
+                            class="border-b-2 border-green">Olmaliq</span>
+                        Savdo</h1>
                     <div
-                        class="search-btn absolute top-0 right-0 flex justify-center items-center p-2 bg-green rounded-r-md cursor-pointer">
-                        <img src="~/assets/icons_svg/Search.svg" alt="search">
+                        class="katalog flex justify-center items-center py-1 px-5 cursor-pointer  gap-2 bg-green  text-white font-semibold rounded-md">
+                        <img src="../../assets/icons_svg/Menu.svg" alt="menu">
+                        <h4>{{ $t('navbar.cat') }}</h4>
+                        <img src="../../assets/icons_svg/ArrowRight.svg" alt="arrow" />
+                    </div>
+                    <div class="search relative  w-7/12">
+                        <input type="text"
+                            class=" border-2 text-black-900 outline-none border-green pl-1 py-1 rounded-md w-full text-sm"
+                            placeholder="Mahsulot izlash...">
+                        <div
+                            class="search-btn absolute top-0 right-0 flex justify-center items-center p-2 bg-green rounded-r-md cursor-pointer">
+                            <img src="~/assets/icons_svg/Search.svg" alt="search">
+                        </div>
+                    </div>
+                    <div class="action-wraper ml-5 flex justify-center items-center gap-4">
+                        <div
+                            class="actions flex-col flex  cursor-pointer items-center hover:text-green transition-all w-9 relative">
+                            <div class="relative m-auto w-1/2">
+                                <img src="~/assets/icons_svg/Heart.svg" class="m-auto " alt="liked">
+                                <div
+                                    class="indicator absolute flex items-center justify-center w-3 h-3 font-semibold text-xs rounded-full bg-red text-white -top-0.4 -right-0.4">
+                                    3</div>
+                            </div>
+                            <div class="text-sm">{{ $t('navbar.sevimli') }}</div>
+                        </div>
+                        <div
+                            class="actions flex-col flex  items-center  cursor-pointer hover:text-green transition-all w-9 relative">
+                            <div class="relative m-auto w-1/2">
+                                <img src="~/assets/icons_svg/Compare.svg" class="m-auto " alt="compare">
+                                <div
+                                    class="indicator absolute flex items-center justify-center w-3 h-3 font-semibold text-xs rounded-full bg-red text-white -top-0.4 -right-0.4">
+                                    8</div>
+                            </div>
+                            <div class="text-sm">{{ $t('navbar.taqqos') }}</div>
+                        </div>
+                        <div
+                            class="actions flex-col flex items-center  cursor-pointer hover:text-green transition-all w-9 relative">
+                            <div class="relative m-auto w-1/2">
+                                <img src="~/assets/icons_svg/Cart.svg" class="m-auto " alt="cart">
+                                <div
+                                    class="indicator absolute flex items-center justify-center w-3 h-3 font-semibold text-xs rounded-full bg-red text-white -top-0.4 -right-0.4">
+                                    2</div>
+                            </div>
+                            <div class="text-sm">{{ $t('navbar.savat') }}</div>
+                        </div>
+                        <router-link to="uz/login">
+                            <div
+                                class="actions flex-col w-7 h-7  flex items-center justify-center rounded-md cursor-pointer items-cent bg-green text-white">
+                                <img src="~/assets/icons_svg/Person.svg" class="m-auto " alt="person">
+                            </div>
+                        </router-link>
+
                     </div>
                 </div>
-                <div class="action-wraper ml-5 flex justify-center items-center gap-4">
-                    <div class="actions flex-col flex  cursor-pointer items-center hover:text-green transition-all w-9 relative">
-                        <div class="relative m-auto w-1/2">
-                            <img src="~/assets/icons_svg/Heart.svg" class="m-auto " alt="liked">
-                            <div class="indicator absolute flex items-center justify-center w-3 h-3 font-semibold text-xs rounded-full bg-red text-white -top-0.4 -right-0.4">3</div>
-                        </div>
-                        <div class="text-sm">{{$t('navbar.sevimli')}}</div>
-                    </div>
-                    <div class="actions flex-col flex  items-center  cursor-pointer hover:text-green transition-all w-9 relative">
-                        <div class="relative m-auto w-1/2">
-                            <img src="~/assets/icons_svg/Compare.svg" class="m-auto " alt="compare">
-                            <div class="indicator absolute flex items-center justify-center w-3 h-3 font-semibold text-xs rounded-full bg-red text-white -top-0.4 -right-0.4">8</div>
-                        </div>
-                        <div class="text-sm">{{$t('navbar.taqqos')}}</div>
-                    </div>
-                    <div class="actions flex-col flex items-center  cursor-pointer hover:text-green transition-all w-9 relative">
-                        <div class="relative m-auto w-1/2">
-                            <img src="~/assets/icons_svg/Cart.svg" class="m-auto " alt="cart">
-                            <div class="indicator absolute flex items-center justify-center w-3 h-3 font-semibold text-xs rounded-full bg-red text-white -top-0.4 -right-0.4">2</div>
-                        </div>
-                        <div class="text-sm">{{$t('navbar.savat')}}</div>
-                    </div>
-                    <div class="actions flex-col w-7 h-7  flex items-center justify-center rounded-md cursor-pointer items-cent bg-green text-white">
-                        <img src="~/assets/icons_svg/Person.svg" class="m-auto " alt="person">
-                    </div>
-                </div>
-            </div>
-        </Container>
+            </Container>
+        </div>
     </div>
-</div>
-    
+
 </template>
 
 <script>
 export default {
     name: "Navbar",
-    data(){
-        return{
+    data() {
+        return {
         }
     },
-//     methods: {
-//             handleFoo() {
-//                 const fooHeader = this.$refs.fooHeader;
-//                 const sticky = fooHeader.offsetTop;
-//                 // console.log(fooHeader);
-//                 window.pageYOffset > sticky ? fooHeader.classList.add('sti') : fooHeader.classList.remove('fixed');
-//             },
-//         },
-// mounted() {
-//             this.$nextTick(() => {
-//                 window.addEventListener('scroll', this.handleFoo);
-//             });
-//         },
 }
 </script>
 
 <style scoped>
-
 </style>
---
+++ pages/index/login.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <Header />
+        <div class="sticky top-0 shadow-sm">
+            <Navbar/>
+        </div>
+        <Container>
+            <div class="flex flex-col items-center justify-center mt-7 font-sans">
+                <img src="~/assets/images/LoginImage.svg" alt="login">
+                <p>Xaridni davom ettirish uchun akkauntingizga kiring yoki <span class="text-green font-semibold cursor-pointer">Olmaliq savdo</span> saytidan <span class="text-green font-semibold cursor-pointer"> <router-link to="ru/logup"> roʻyxatdan</router-link></span> oʻting !!! </p>
+                <h1 class="font-semibold text-4xl my-4 text-green ">{{$t("registration.welcome")}}</h1>
+                <LoginPage />
+                </div>
+        </Container>
+        <Footer/>
+    </div>
+</template>
+
+<script>
+import LoginPage from '~/components/regstration/LoginPage.vue';
+import Navbar from '~/components/navbar/Navbar.vue';
+import Container from '~/components/Container.vue';
+import Footer from '~/components/footer/Footer.vue';
+import Header from '~/components/header/header.vue'
+export default {
+    name: "Registration",
+    components: { LoginPage, Header, Navbar, Container, Footer }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
